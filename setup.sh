0342fcb0c1b6fc4fb961f4cd462bcc09c5ee7536
Adam Barla <barlaada@fit.cvut.cz>
2025-02-23 18:13:43 +0100

    feat: setup script

---
+++ setup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DOTFILES_DIR="$HOME/dotfiles"
+
+if [ ! -d "$DOTFILES_DIR" ]; then
+	echo "err: missing repo at $DOTFILES_DIR"
+	exit 1
+fi
+
+DOTFILES=(
+	.zshrc
+	.gitconfig
+	.config/nvim/init.vim
+)
+
+alias dotfiles='/usr/bin/git --git-dir=$DOTFILES_DIR --work-tree=$HOME'
+
+dotfiles checkout
+dotfiles config --local statues.showUntrackedFiles no
+
+for file in "${DOTFILES[@]}"; do
+    SRC="$DOTFILES_DIR/$file"
+    DEST="$HOME/$file"
+
+	# backup existing
+    if [ -e "$DEST" ]; then
+        mv "$DEST" "$DEST.backup"
+    fi
+
+    mkdir -p "$(dirname "$DEST")" # create destination
+
+    ln -s "$SRC" "$DEST"
+    echo "Symlinked $file"
+done
